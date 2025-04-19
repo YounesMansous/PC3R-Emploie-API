608d404829a8bc14039235153c26fa5099d3d186
toureydaou <ttoureydaou@gmail.com>
2025-04-19 15:16:23 +0200

    récupérations des modes de transports

--- BackEnd/api/controllers/lines/lines.go
+++ BackEnd/api/controllers/lines/lines.go
@@ -1,17 +1,51 @@
 package lines
 
 import (
+	"api/models"
+	"api/utils/database"
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
-
 
 func GetTransportModesHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Printf("endpoint récupérant les modes de transports")
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var modes []models.TypeTransportsJSON
+
+	rows, err := database.DB.Query(context.Background(), "SELECT DISTINCT type FROM lines ORDER BY type desc")
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Database error", http.StatusInternalServerError)
+		return
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var mode models.TypeTransportsJSON
+		err := rows.Scan(&mode.TYPE)
+		if err != nil {
+			fmt.Printf("Erreur lors de la lecture des résultats %s \n", err)
+			continue
+		}
+		modes = append(modes, mode)
+	}
+
+	response := map[string][]models.TypeTransportsJSON{
+		"modes": modes,
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
 }
 
 func GetTransportModeLinesIdsHandler(w http.ResponseWriter, r *http.Request) {
---
+++ BackEnd/api/models/lines.go
@@ -0,0 +1,5 @@
+package models
+
+type TypeTransportsJSON struct {
+	TYPE string `json:"type"`
+}
